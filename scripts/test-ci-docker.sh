105ee088f4e1e066b86fad76503e3450e07d7279
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2025-12-09 12:46:24 -0500

    Fix Docker script

--- scripts/test-ci-docker.sh
+++ scripts/test-ci-docker.sh
@@ -9,7 +9,7 @@ BLUE='\033[0;34m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")/.."
 
 if ! docker ps > /dev/null 2>&1; then
     echo -e "${RED}Docker not running${NC}"
