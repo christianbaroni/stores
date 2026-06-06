62ff90977f1b01e256495bbb171a024a24265b36
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2026-06-06 14:56:27 -0400

    Clean up `omitStoreMethods`

--- packages/core/src/utils/persistUtils.ts
+++ packages/core/src/utils/persistUtils.ts
@@ -1,25 +1,27 @@
 import { StoresError } from '../errors';
 import { logger } from '../logger';
-import { StorageValue } from '../storage/storageTypes';
+import type { StorageValue } from '../storage/storageTypes';
+import { isPlainObject } from '../types/utils';
+import { nullObject } from './core';
 import { replacer, reviver } from './serialization';
 
 /**
  * Default partialize function if none is provided. It omits top-level store
  * methods and keeps all other state.
  */
-export function omitStoreMethods<S, PersistedState extends Partial<S>>(state: S): PersistedState {
-  if (state !== null && typeof state === 'object') {
-    const result: Record<string, unknown> = {};
-    for (const [key, val] of Object.entries(state)) {
-      if (typeof val !== 'function') {
-        result[key] = val;
-      }
-    }
-    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    return result as PersistedState;
+export function omitStoreMethods<S, PersistedState extends Partial<S> = Partial<S>>(state: S): PersistedState;
+export function omitStoreMethods(state: unknown): unknown {
+  if (!isPlainObject(state)) return state;
+
+  const result: Record<string, unknown> = nullObject();
+  const keys = Object.keys(state);
+
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    const value = state[key];
+    if (typeof value !== 'function') result[key] = value;
   }
-  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  return state as unknown as PersistedState;
+  return result;
 }
 
 export function defaultSerializeState<PersistedState>(storageValue: StorageValue<PersistedState>, shouldUseReplacer: boolean): string {
